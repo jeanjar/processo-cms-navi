ca79858abeef2a86312740f3e91357720d2d702d
Jean Jar <jeanjpa@gmail.com>
2018-06-28 16:05:14 -0300

    init

---
+++ scripts/processElementData.js
@@ -0,0 +1,34 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_aTaMkXr8EeiEQ7RNJ3NSGwActivity"]="./../../Processo CMS NAVI/deliveryprocesses/resources/Processo CMS NAVI_9192A1CD_9192a1cd_Activity.jpeg"
+contentPage.processPage.elementUrls["_U_pv03sDEeiEQ7RNJ3NSGw"] = ["Reunião de revisão da Sprint","Processo CMS NAVI/deliveryprocesses/sprint_review_meeting_72544BFF.html"];
+contentPage.processPage.elementUrls["_U_pv0HsDEeiEQ7RNJ3NSGw"] = ["Desenvolver incremento","Processo CMS NAVI/deliveryprocesses/desenvolver_incremento_10438214.html"];
+contentPage.processPage.elementUrls["_RdMVoXsDEeiEQ7RNJ3NSGw"] = ["Sprint","Processo CMS NAVI/deliveryprocesses/Sprint_C5C71C50.html"];
+contentPage.processPage.elementUrls["_U_pvxHsDEeiEQ7RNJ3NSGw"] = ["Product Backlog","Processo CMS NAVI/deliveryprocesses/product_backlog_594B1ACC.html"];
+contentPage.processPage.elementUrls["_U_pvx3sDEeiEQ7RNJ3NSGw"] = ["ScrumMaster","Processo CMS NAVI/deliveryprocesses/scrummaster_BB5BE4B7.html"];
+contentPage.processPage.elementUrls["_U_pvyXsDEeiEQ7RNJ3NSGw"] = ["Scrum Team","Processo CMS NAVI/deliveryprocesses/scrum_team_287ADCFB.html"];
+contentPage.processPage.elementUrls["_U_pvzXsDEeiEQ7RNJ3NSGw"] = ["Sprint Backlog","Processo CMS NAVI/deliveryprocesses/sprint_backlog_ED0CBF1A.html"];
+contentPage.processPage.elementUrls["_U_pvy3sDEeiEQ7RNJ3NSGw"] = ["Incremento de Produto Potencialmente Utilizável.","Processo CMS NAVI/deliveryprocesses/potentially_shippable_product_incremement_7FEDC6D6.html"];
+contentPage.processPage.elementUrls["_U_pvyHsDEeiEQ7RNJ3NSGw"] = ["Planejamento da Sprint","Processo CMS NAVI/deliveryprocesses/Planejamento da iteração_1DDCFCEB.html"];
+contentPage.processPage.elementUrls["_U_pvzHsDEeiEQ7RNJ3NSGw"] = ["Scrum Team","Processo CMS NAVI/deliveryprocesses/scrum_team_E26EDF0A.html"];
+contentPage.processPage.elementUrls["_U_pv2nsDEeiEQ7RNJ3NSGw"] = ["Sprint Backlog","Processo CMS NAVI/deliveryprocesses/sprint_backlog_929E3A78.html"];
+contentPage.processPage.elementUrls["_U_pvxnsDEeiEQ7RNJ3NSGw"] = ["Reunião de planejamento Sprint","Processo CMS NAVI/deliveryprocesses/sprint_planning_meeting_52820EF2.html"];
+contentPage.processPage.elementUrls["_U_pv1nsDEeiEQ7RNJ3NSGw"] = ["Retrospectiva do Sprint","Processo CMS NAVI/deliveryprocesses/sprint_retrospective_CE0C5859.html"];
+contentPage.processPage.elementUrls["_U_pv13sDEeiEQ7RNJ3NSGw"] = ["Scrum Team","Processo CMS NAVI/deliveryprocesses/scrum_team_36E62E1E.html"];
+contentPage.processPage.elementUrls["_U_pvxXsDEeiEQ7RNJ3NSGw"] = ["Sprint Backlog","Processo CMS NAVI/deliveryprocesses/sprint_backlog_63E8FADC.html"];
+contentPage.processPage.elementUrls["_U_pvznsDEeiEQ7RNJ3NSGw"] = ["Desenvolvimento","Processo CMS NAVI/deliveryprocesses/Desenvolvimento_DBA5D330.html"];
+contentPage.processPage.elementUrls["_U_pv0nsDEeiEQ7RNJ3NSGw"] = ["ScrumMaster","Processo CMS NAVI/deliveryprocesses/scrummaster_97A763A.html"];
+contentPage.processPage.elementUrls["_aTaMkXr8EeiEQ7RNJ3NSGw"] = ["Processo CMS NAVI","Processo CMS NAVI/deliveryprocesses/Processo CMS NAVI_9192A1CD.html"];
+contentPage.processPage.elementUrls["_U_pv2XsDEeiEQ7RNJ3NSGw"] = ["Revisão da Sprint","Processo CMS NAVI/deliveryprocesses/Revisão da Sprint_A4052662.html"];
+contentPage.processPage.elementUrls["_U_pvw3sDEeiEQ7RNJ3NSGw"] = ["Planejar Release","Processo CMS NAVI/deliveryprocesses/release_planning_F6CA0298.html"];
